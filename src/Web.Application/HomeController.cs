c3568c9d1a245d6653c0b74c27ba463f30ee66e4
hazzik <hazzik@gmail.com>
2011-04-16 23:50:33 +0600

    Added empty home controller

---
+++ src/Web.Application/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Web.Application
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return new EmptyResult();
+        }
+    }
+}
