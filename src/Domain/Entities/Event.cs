75e6fd55e09be1a4281a59e5cb96d2850539f20c
hazzik <hazzik@gmail.com>
2011-07-12 21:05:22 +0600

    Added event and user entities

---
+++ src/Domain/Entities/Event.cs
@@ -0,0 +1,27 @@
+﻿namespace Domain.Entities
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Event : IEntity
+    {
+        private readonly ICollection<User> participants = new List<User>();
+
+        public virtual DateTime StartedAt { get; set; }
+        public virtual DateTime FinishedAt { get; set; }
+        public virtual string Place { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+
+        public IEnumerable<User> Participants
+        {
+            get { return participants; }
+        }
+
+        #region IEntity Members
+
+        public virtual int Id { get; private set; }
+
+        #endregion
+    }
+}
---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+namespace Domain.Entities
+{
+    public class User : IEntity
+    {
+        public virtual string Name { get; set; }
+
+        #region IEntity Members
+
+        public virtual int Id { get; set; }
+
+        #endregion
+    }
+}
