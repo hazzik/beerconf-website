2bfd6aea5a659ebafa076317d123c68d0a007b6f
hazzik <hazzik@gmail.com>
2011-07-20 15:05:53 +0600

    Added test for forms and handlers integration

---
+++ src/Web.Application.Tests/CheckFormsAndHandlersIntegration.cs
@@ -0,0 +1,38 @@
+﻿namespace Web.Application.Tests
+{
+	using System;
+	using System.Linq;
+	using System.Reflection;
+	using Xunit;
+
+	public class CheckFormsAndHandlersIntegration
+	{
+		[Fact]
+		public void AllFormsShouldHaveOneAndOnlyOneHandler()
+		{
+			Type[] types = Assembly.Load("BeerConf.Web.Application")
+				.GetTypes();
+
+			Type[] forms = types
+				.Where(x => x.GetInterface("IForm") != null)
+				.Where(x => x.IsPublic && x.IsAbstract == false)
+				.ToArray();
+
+			Type[] handlers = types
+				.Where(x => x.GetInterface("IFormHandler`1") != null)
+				.Where(x => x.IsPublic && x.IsAbstract == false)
+				.ToArray();
+
+			foreach (Type form in forms)
+			{
+				var concreteHandlers = handlers
+					.Where(x => x.GetInterface("IFormHandler`1").GetGenericArguments().First() == form)
+					.ToArray();
+				Console.WriteLine("{0}", form);
+				foreach (var concreteHandler in concreteHandlers)
+					Console.WriteLine("\thandled by {0}", concreteHandler);
+				Assert.Equal(concreteHandlers.Length, 1);
+			}
+		}
+	}
+}
