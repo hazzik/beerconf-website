07f35333645f7d509690a8626cae20ae90ba732e
hazzik <hazzik@gmail.com>
2011-11-13 14:16:40 +0600

    Updated Brandy

---
+++ src/Infrastructure.NHibernate/Conventions/PrimaryKeyGeneratorConvention.cs
@@ -0,0 +1,13 @@
+﻿namespace BeerConf.Infrastructure.NHibernate.Conventions
+{
+    using FluentNHibernate.Conventions;
+    using FluentNHibernate.Conventions.Instances;
+
+    public class PrimaryKeyGeneratorConvention : IIdConvention
+    {
+        public void Apply(IIdentityInstance instance)
+        {
+            instance.GeneratedBy.HiLo("100");
+        }
+    }
+}
---
+++ src/Migrations/Migration201111131404.cs
@@ -0,0 +1,15 @@
+﻿namespace BeerConf.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(201111131404)]
+    public class Migration201111131404 : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("hibernate_unique_key")
+                .WithColumn("next_hi").AsInt32().PrimaryKey().NotNullable();
+            Insert.IntoTable("hibernate_unique_key").Row(new { next_hi = 0 });
+        }
+    }
+}
