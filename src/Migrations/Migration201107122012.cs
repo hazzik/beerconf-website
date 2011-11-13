47eadc62ea2038310b69c4b3b2bda48d7d610d30
hazzik <hazzik@gmail.com>
2011-11-13 14:38:39 +0600

    Removed identity specification on columns

--- src/Migrations/Migration201107122012.cs
+++ src/Migrations/Migration201107122012.cs
@@ -12,7 +12,7 @@
                 .WithColumn("USER_ID").AsInt32().PrimaryKey();
 
             Create.Table("EVENTS")
-                .WithColumn("EVENT_ID").AsInt32().PrimaryKey().Identity().References("FK_EVENT_PARTICIPANTS_EVENTS", "EVENT_PARTICIPANTS", new[] {"EVENT_ID"})
+                .WithColumn("EVENT_ID").AsInt32().PrimaryKey().References("FK_EVENT_PARTICIPANTS_EVENTS", "EVENT_PARTICIPANTS", new[] {"EVENT_ID"})
                 .WithColumn("STARTED_AT").AsDateTime().NotNullable()
                 .WithColumn("FINISHED_AT").AsDateTime().NotNullable()
                 .WithColumn("PLACE").AsString().NotNullable()
@@ -20,7 +20,7 @@
                 .WithColumn("DESCRIPTION").AsString().NotNullable();
 
             Create.Table("USERS")
-                .WithColumn("USER_ID").AsInt32().PrimaryKey().Identity().References("FK_EVENT_PARTICIPANTS_USERS", "EVENT_PARTICIPANTS", new[] {"USER_ID"});
+                .WithColumn("USER_ID").AsInt32().PrimaryKey().References("FK_EVENT_PARTICIPANTS_USERS", "EVENT_PARTICIPANTS", new[] {"USER_ID"});
         }
     }
 }
