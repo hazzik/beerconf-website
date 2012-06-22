702e10d41869a6cc96027ae23c969879e03e9ff3
eskat0n <muyou.prj@gmail.com>
2012-06-22 15:12:53 +0600

    error pages controller added

---
+++ src/Web.Application/Errors/ErrorsController.cs
@@ -0,0 +1,17 @@
+﻿namespace BeerConf.Web.Application.Errors
+{
+    using System.Web.Mvc;
+
+    public class ErrorsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
